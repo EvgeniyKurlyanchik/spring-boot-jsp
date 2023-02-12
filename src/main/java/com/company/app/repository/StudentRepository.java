de1cff6e7287bc3cd8d0ec23813fa58ae4e8ced5
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2023-02-13 00:45:55 +0300

    студент basic classes

---
+++ src/main/java/com/company/app/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.company.app.repository;
+
+
+import com.company.app.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
